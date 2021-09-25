87d243ade5a25684d4f6f13d4d25570a2f3c48d7
Đặng Ngọc Tuân <67722406+tuanco98@users.noreply.github.com>
2021-09-25 15:34:24 +0700

    update .env

--- src/BrickSDK.ts
+++ src/BrickSDK.ts
@@ -1,9 +1,11 @@
+import { CONFIG_PROVIDER } from "./configs";
+
 const { BrickSDK } = require('@brickglobal/brick-sdk');
 export let brickSDKv2: any
 export const brickSDK_v2 = async (apiKey: string) => {
   const bricksdk_v2 = new BrickSDK({
     apiKey, 
-    provider: "http://139.99.121.150:6699/" 
+    provider: CONFIG_PROVIDER 
   }).v2
   return bricksdk_v2;
 }
--- src/configs.ts
+++ src/configs.ts
@@ -3,3 +3,6 @@ config();
 
 if (!process.env.PORT) throw new Error(`PORT must be provided`);
 export const CONFIG_PORT = process.env.PORT;
+
+if (!process.env.PROVIDER) throw new Error(`PROVIDER must be provided`);
+export const CONFIG_PROVIDER = process.env.PROVIDER;
